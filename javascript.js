40e882c923eb5fe6f004c0afa3dcc7fc4773011c
soso2410 <sophiavfa@gmail.com>
2025-08-04 12:13:48 -0300

    closes #5 - Botão tipo submit

--- javascript.js
+++ javascript.js
@@ -8,6 +8,12 @@ var courses = [
     { id: 3, name: "Psicologia" }
 ];
 
+// OnLoad
+window.onload = function () {
+    loadStudents();
+};
+
+
 // Load all students
 function loadStudents() {
     for (let student of students) {
@@ -15,14 +21,10 @@ function loadStudents() {
     }
 }
 
-// OnLoad
-window.onload = function () {
-    loadStudents();
-};
 
 // Add new row to the table
 function addNewRow(student) {
-    console.log (student)
+    console.log(student)
     var table = document.getElementById("studentsTable");
     var newRow = table.insertRow();
 
@@ -53,17 +55,16 @@ function addNewRow(student) {
     } else if (student.shift == "noite") {
         shiftBadge = "<span class='badge bg-danger'>N</span>";
     }
-    newRow.insertCell().innerHTML = shiftBadge; 
+    newRow.insertCell().innerHTML = shiftBadge;
 }
 
 // Função para cadastrar aluno
 function cadastrarAluno() {
     var name = document.getElementById("inputName").value;
     var email = document.getElementById("inputEmail").value;
-    var tel = document.getElementById("tel").value.replace(/\D/g, ''); 
+    var tel = document.getElementById("tel").value.replace(/\D/g, '');
     var course = document.getElementById("Selectcourse").value;
-    var shift = document.querySelector('input[name="radioDefault"]:checked').value; 
-    console.log (shift)
+    var shift = document.querySelector('input[name="radioDefault"]:checked').value;
 
     var newStudent = {
         id: students.length + 1,
@@ -71,11 +72,11 @@ function cadastrarAluno() {
         email: email,
         tel: tel,
         course: parseInt(course),
-        shift: shift 
+        shift: shift
     };
 
     students.push(newStudent);
     addNewRow(newStudent);
+
+    document.getElementById("formStudents").reset();
 }
-
-
